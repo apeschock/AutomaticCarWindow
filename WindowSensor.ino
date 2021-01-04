f8f28412ea520c3ccaf4e5577975708e5783b31b
amp294 <amp294@pitt.edu>
2021-01-03 23:59:10 -0500

    Finished

--- WindowSensor.ino
+++ WindowSensor.ino
@@ -2,86 +2,110 @@
 
 //Window Motor Auto System
 
-const int winUp= 2; //window up pin
-const int winDownAuto = 3; //window auto down signal pin
+const int winUp = 3; //window up pin
+const int winDownAuto = 2; //window auto down signal pin
 const int winDown = 4; //window down move
 
-const int currentInPin = 0; //Analog pin 0
+const int currentInPin = A0; //Analog pin 0
 const int sensitivity = 100;
 const int offsetV = 2500;
-const double MAX_CURRENT = 15;
+const double MAX_CURRENT = 9;
 const unsigned long TIMEOUT_OFFSET = 6000;
-const double LOW_THRESHOLD = .95;
-const double HIGH_THRESHOLD = 1.05;
+const int AUTO_UP_TIMER = 500;
+const int SLEEP_TIME = 5000;
+unsigned long stopTime = 0;
 
-volatile boolean movingUp = false;
-volatile boolean movingDown = false;
+volatile boolean awake = false;
 
 void setup() {
-  // put your setup code here, to run once:
-  pinMode(currentInPin, INPUT);
-  pinMode(winUp, INPUT); //this will change on the fly
-  pinMode(winDownAuto, INPUT);
-  pinMode(winDown, OUTPUT);
+	// put your setup code here, to run once:
+	pinMode(currentInPin, INPUT);
+	pinMode(winUp, INPUT_PULLUP); //this will change on the fly
+	pinMode(winDownAuto, INPUT_PULLUP);
+	pinMode(winDown, OUTPUT);
+	digitalWrite(winDown, HIGH);
+	Serial.begin(9600);
 
-  //active when pulled to ground
-  attachInterrupt(winUp, autoUp, FALLING);
-  attachInterrupt(winDownAuto, autoDown, FALLING);
-  
+	//active when pulled to ground
+	attachInterrupt(digitalPinToInterrupt(winUp), wakeUp, FALLING);
+	attachInterrupt(digitalPinToInterrupt(winDownAuto), wakeUp, FALLING);
 }
 
 void loop() {
-  //set a time to watch how long to be running
-  unsigned long startTime = millis();
-  if(movingUp){
-    pinMode(winUp, OUTPUT);
-    digitalWrite(winUp, LOW);
-    double lowBound = getCurrent() * LOW_THRESHOLD;
-    double highBound = getCurrent() * HIGH_THRESHOLD;
-    while(millis() < startTime + TIMEOUT_OFFSET && getCurrent() < MAX_CURRENT && getCurrent() > lowBound && getCurrent() < highBound){
-      //wait here til its reached max
-      if(movingDown){
-        movingUp = false;
-        movingDown = false;
-        break;
-      }
-    }
-    digitalWrite(winUp, HIGH);
-    pinMode(winUp, INPUT);
-    movingUp = false;
-  }else if(movingDown){
-    pinMode(winDown, OUTPUT);
-    digitalWrite(winDown, LOW);
-    double lowBound = getCurrent() * LOW_THRESHOLD;
-    double highBound = getCurrent() * HIGH_THRESHOLD;
-    while(millis() < startTime + TIMEOUT_OFFSET && getCurrent() < MAX_CURRENT && getCurrent() > lowBound && getCurrent() < highBound){
-      //wait here til its reached max or see if cancelled
-      if(movingUp){
-        //user said stop
-        movingUp = false;
-        movingDown = false;
-        break;
-      }
-    }
-    digitalWrite(winDown, HIGH);
-    pinMode(winDown, INPUT);
-    movingDown = false;
-  }
+	while (awake) {
+		Serial.println("Woke");
+		//start timer and read pins
+		unsigned long startTime = millis();
 
-  //sleep til next interrupt
-  LowPower.powerDown(SLEEP_FOREVER, ADC_OFF, BOD_OFF);
+		float lowBound = .0f;
+		float highBound = .0f;
+
+		if (!digitalRead(winUp)) {
+			//long press
+			while (!digitalRead(winUp)) {
+				unsigned long currentTime = millis();
+				Serial.println(currentTime - startTime);
+				//wait a few millis before engaging auto up
+				if ((currentTime - startTime) > AUTO_UP_TIMER) {
+					pinMode(winUp, OUTPUT);
+					pinMode(winDown, INPUT_PULLUP);
+					delay(200);
+					digitalWrite(winUp, LOW);
+
+					//make sure dont trip watchdog timer/current
+					while (millis() < (currentTime + TIMEOUT_OFFSET) && (getCurrent() < MAX_CURRENT)) {
+						Serial.println("Up");
+						if (!digitalRead(winDown)) {
+							break;
+						}
+					}
+					//go back to input and stop commanding motor
+					Serial.println("StopUp");
+					digitalWrite(winUp, HIGH);
+					pinMode(winUp, INPUT_PULLUP);
+					pinMode(winDown, OUTPUT);
+					stopTime = millis();
+				}
+			}
+		}
+		else if (!digitalRead(winDownAuto)) { //if autodown button pressed
+			unsigned long currentTime = millis();
+			//start moving down
+			digitalWrite(winDown, LOW);
+			//watchdogs
+			while ((millis() < currentTime + TIMEOUT_OFFSET) && (getCurrent() < MAX_CURRENT)) {
+				Serial.println("Down");
+				if (!digitalRead(winUp)) {
+					break;
+				}
+			}
+			//stop moving
+			Serial.println("StopDown");
+			digitalWrite(winDown, HIGH);
+			pinMode(winUp, INPUT_PULLUP);
+			stopTime = millis();
+		}
+		else {
+			//sleep till next interrupt after timer
+			unsigned long currentTime = millis();
+			if (stopTime + SLEEP_TIME < currentTime) {
+				awake = false;
+				stopTime = currentTime;
+				Serial.println("Sleeping");
+				Serial.flush();
+				LowPower.powerDown(SLEEP_FOREVER, ADC_OFF, BOD_OFF);
+			}
+		}
+	}
 }
 
-void autoUp(){
-  movingUp = true;
+void wakeUp() {
+	awake = true;
 }
 
-void autoDown(){
-  movingDown = true;
-}
-
-double getCurrent(){
-  int adc = analogRead(currentInPin);
-  double volt = (adc / 1024.0) * 5000; //mV reading
-  return ((volt - offsetV) / sensitivity); //return current
+double getCurrent() {
+	int adc = analogRead(currentInPin);
+	float volt = (adc / 1024.0f) * 5000.0f; //mV reading
+	float current = ((volt - offsetV) / sensitivity); //return current
+	return (current < 0 ? -current : current); //make current positive
 }
